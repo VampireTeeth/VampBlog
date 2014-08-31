2196f72d53b0d23297e5144e724676b879a3d06d
VampireTeeth <vampireteeth001@gmail.com>
2014-08-31 12:06:35 +1000

    Post and PostTest added

---
+++ src/main/java/org/vampireteeth/domain/Post.java
@@ -0,0 +1,95 @@
+package org.vampireteeth.domain;
+
+import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+
+public class Post {
+
+	@Id
+	private String id;
+
+	private String title;
+
+	private String content;
+
+  private List<String> tags;
+
+	private List<String> comments;
+
+	private Calendar createdOn;
+
+  public Post(String title, String content) {
+    this.title = title;
+    this.content = content;
+    this.tags = new LinkedList<String>();
+    this.comments = new LinkedList<String>();
+    this.createdOn = Calendar.getInstance();
+  }
+
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+  /**
+   * @set the title
+   */
+  public Post setTitle(String title){
+    this.title = title;
+    return this;
+  }
+
+
+	/**
+	 * @return the content
+	 */
+	public String getContent() {
+		return content;
+	}
+
+	/**
+	 * @param content the content to set
+	 */
+	public Post setContent(String content) {
+		this.content = content;
+    return this;
+	}
+
+	/**
+	 * @return the tags
+	 */
+	public List<String> getTags() {
+		return tags;
+	}
+
+  public Post addTag(String tag) {
+    this.tags.add(tag);
+    return this;
+  }
+
+	/**
+	 * @return the comments
+	 */
+	public List<String> getComments() {
+		return comments;
+	}
+
+  public Post addComment(String comment) {
+    this.comments.add(comment);
+    return this;
+  }
+
+	/**
+	 * @return the createdOn
+	 */
+	public Calendar getCreatedOn() {
+		return createdOn;
+	}
+
+}
---
+++ src/test/java/org/vampireteeth/domain/PostTest.java
@@ -0,0 +1,73 @@
+package org.vampireteeth.domain;
+
+import java.util.Calendar;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PostTest {
+
+	private String title;
+	private String content;
+	private Post post;
+
+	@Before
+	public void setUp() {
+		this.title = "The google earth";
+		this.content = "This is a short content";
+		this.post = new Post(this.title, this.content);
+	}
+
+	@Test
+	public void testGetTitle() {
+		String title = this.post.getTitle();
+		Assert.assertEquals(this.title, title);
+	}
+
+  @Test
+	public void testGetContent() {
+    String content = this.post.getContent();
+    Assert.assertEquals(this.content, content);
+	}
+
+	@Test
+	public void testGetTagsEmpty() {
+		Assert.assertEquals(0, this.post.getTags().size());
+	}
+
+	@Test
+	public void testGetCommentsEmpty() {
+		Assert.assertEquals(0, this.post.getComments().size());
+	}
+
+	@Test
+	public void testAddTag() {
+		String tag = "First tag";
+		this.post.addTag(tag);
+		Assert.assertEquals(1, this.post.getTags().size());
+		Assert.assertEquals(tag, this.post.getTags().get(0));
+	}
+
+	@Test
+	public void testAddComment() {
+		String comment = "First comment";
+		this.post.addComment(comment);
+		Assert.assertEquals(1, this.post.getComments().size());
+		Assert.assertEquals(comment, this.post.getComments().get(0));
+	}
+
+	@Test
+	public void testGetCreatedOn() {
+		Calendar expected = Calendar.getInstance();
+		Calendar actual = this.post.getCreatedOn();
+		Assert.assertEquals(expected.get(Calendar.DAY_OF_YEAR),
+				actual.get(Calendar.DAY_OF_YEAR));
+		Assert.assertEquals(expected.get(Calendar.HOUR),
+				actual.get(Calendar.HOUR));
+		Assert.assertEquals(expected.get(Calendar.MINUTE),
+				actual.get(Calendar.MINUTE));
+		Assert.assertEquals(expected.get(Calendar.SECOND),
+				actual.get(Calendar.SECOND));
+	}
+}
