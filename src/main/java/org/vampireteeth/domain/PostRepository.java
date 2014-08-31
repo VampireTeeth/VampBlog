0243c70e183521b82a303440be3d8733e1daa89c
VampireTeeth <vampireteeth001@gmail.com>
2014-08-31 22:02:16 +1000

    PostRepository added

---
+++ src/main/java/org/vampireteeth/domain/PostRepository.java
@@ -0,0 +1,14 @@
+package org.vampireteeth.domain;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+public interface PostRepository extends MongoRepository<Post, String> {
+
+  List<Post> findAll();
+
+  List<Post> findAllByTitle(String title);
+
+}
